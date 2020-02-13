0deaab33f8acf9bd885a188b79645973e96dd193
GeorgeWahba <george-wahba@hotmail.com>
2020-02-13 11:49:34 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,6 @@
+var info ={
+    name:"Zara", score:36, date:"21 januari 2016", friends:"Mika, Josef, Maria, Sumail, Lotte", Gekwalificeerd:"waar"
+}
+var json = JSON.stringify(info);
+
+console.log(json);
